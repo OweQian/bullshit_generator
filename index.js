00fce0ed5709acbea27d3b05d778014d7b7a0829
qian wang <1510106069@qq.com>
2023-07-19 23:49:59 +0800

    feat: process.argv传参

--- index.js
+++ index.js
@@ -3,9 +3,58 @@ import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
 import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
-import moment from "moment";
+import commandLineArgs from 'command-line-args';
+import commandLineUsage from 'command-line-usage'
+import moment from 'moment';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const cmdType = {
+  title: 'title',
+  min: 'min',
+  max: 'max',
+  help: 'help',
+}
+
+const optionDefinitions = [{
+  name: cmdType.help,
+}, {
+  name: cmdType.title,
+  type: String,
+}, {
+  name: cmdType.min,
+  type: Number,
+}, {
+  name: cmdType.max,
+  type: Number,
+}]
+
+const sections = [
+  {
+    header: '仿狗屁不通文章生成器',
+    content: '生成随机的文章段落用于测试'
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'title',
+        typeLabel: '{underline string}',
+        description: '文章的主题'
+      },
+      {
+        name: 'min',
+        typeLabel: '{underline number}',
+        description: '文章的最小字数'
+      },
+      {
+        name: 'max',
+        typeLabel: '{underline number}',
+        description: '文章的最大字数'
+      },
+    ]
+  }
+]
 
 function loadCorpus(src) {
   const path = resolve(__dirname, src);
@@ -13,7 +62,7 @@ function loadCorpus(src) {
   return JSON.parse(data);
 }
 
-function saveCorpus(title, article) {
+function saveToFile(title, article) {
   const outputDir = resolve(__dirname, 'output');
   const time = moment().format('|YYYY-MM-DD|HH:mm:ss');
   const outputFile = resolve(outputDir, `${title}${time}.txt`);
@@ -26,10 +75,14 @@ function saveCorpus(title, article) {
 }
 
 const corpus = loadCorpus('corpus/data.json');
+const options = commandLineArgs(optionDefinitions);
+const usage = commandLineUsage(sections);
+if (cmdType.help in options) {
+  console.log(usage);
+} else {
+  const title = options.title || createRandomPicker(corpus.title)();
+  const article = generate(title, {corpus, ...options});
+  const output = saveToFile(title, article);
+  console.log(`生成成功！文章保存于：${output}`);
+}
 
-const pickTitle = createRandomPicker(corpus.title);
-const title = pickTitle();
-
-const article = generate(title, {corpus});
-
-console.log(`${title}\n\n    ${article.join('\n    ')}`);
