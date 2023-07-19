18af6bafd6079e5a7e9f913d57cf286755b5b17f
qian wang <1510106069@qq.com>
2023-07-19 22:30:26 +0800

    feat: fs生成文件

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import {readFileSync} from 'fs';
+import {readFileSync, existsSync, mkdirSync, writeFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
 import {generate} from './lib/generator.js';
 import {createRandomPicker} from './lib/random.js';
+import moment from "moment";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -10,6 +11,18 @@ function loadCorpus(src) {
   const path = resolve(__dirname, src);
   const data = readFileSync(path, {encoding: 'utf-8'});
   return JSON.parse(data);
+}
+
+function saveCorpus(title, article) {
+  const outputDir = resolve(__dirname, 'output');
+  const time = moment().format('|YYYY-MM-DD|HH:mm:ss');
+  const outputFile = resolve(outputDir, `${title}${time}.txt`);
+  if (!existsSync(outputDir)) {
+    mkdirSync(outputDir);
+  }
+  const text = `${title}\n\n    ${article.join('\n    ')}`;
+  writeFileSync(outputFile, text);
+  return outputFile;
 }
 
 const corpus = loadCorpus('corpus/data.json');
