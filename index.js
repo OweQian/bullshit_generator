7f3906f5834e0dd96c89909d589bdc69e4bc6589
qian wang <1510106069@qq.com>
2023-07-19 21:22:35 +0800

    feat: 生成文章

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import {readFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 import {dirname, resolve} from 'path';
-// readFile('./corpus/data.json', {encoding: 'utf-8'}, (err, data) => {
-//   if (!err) {
-//     console.log(data);
-//   } else {
-//     console.error(err);
-//   }
-// });
-const url = import.meta.url; // 获取当前脚本文件的url
-console.log(import.meta);
-const path = resolve(dirname(fileURLToPath(url)), 'corpus/data.json');
-const data = readFileSync(path, {encoding: 'utf-8'});
-const corpus = JSON.parse(data);
-console.log(corpus);
+import {generate} from './lib/generator.js';
+import {createRandomPicker} from './lib/random.js';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+function loadCorpus(src) {
+  const path = resolve(__dirname, src);
+  const data = readFileSync(path, {encoding: 'utf-8'});
+  return JSON.parse(data);
+}
+
+const corpus = loadCorpus('corpus/data.json');
+
+const pickTitle = createRandomPicker(corpus.title);
+const title = pickTitle();
+
+const article = generate(title, {corpus});
+
+console.log(`${title}\n\n    ${article.join('\n    ')}`);
